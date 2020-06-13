cb8b9da46dcd0ceaeb06903daf7dc977f8957854
fedorov <serstalk@gmail.com>
2020-06-13 23:35:09 +0300

    Task2 is done

--- src/HomeWork2/Task2.java
+++ src/HomeWork2/Task2.java
@@ -2,7 +2,7 @@ package HomeWork2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 //Задание 2: Выведите на экран список различных слов файла,
 // отсортированный по возрастанию их длины (компаратор сначала по длине слова,
@@ -12,12 +12,34 @@ public class Task2 {
     public static void main(String[] args) {
         File textFile = new File("src/HomeWork2/words.txt");
         try (Scanner scanner = new Scanner(textFile)) {
-            String[] words = scanner.nextLine().split("\\s+");
+            String[] words = null;
 
+
+            while (scanner.hasNext()){
+                words = scanner.nextLine().split("\\s+");
+                List<String> stringList = new ArrayList<>(Arrays.asList(words));
+                stringList.sort(new StringComparator());
+                System.out.println(stringList);
+            }
 
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
+
 }
+
+class StringComparator implements Comparator<String>{
+
+    @Override
+    public int compare(String o1, String o2) {
+        if(o1.length() > o2.length())
+            return 1;
+        else if(o1.length() < o2.length())
+            return -1;
+        else
+            return 0;
+    }
+}
+
